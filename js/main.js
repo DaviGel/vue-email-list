21d3eda7764d2d54801a91a1e3b01309c1efe955
DaviGel <geloso.davide@gmail.com>
2023-12-05 16:40:54 +0100

    feat: esercizio + bonus

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail',
+      emailAddress: [],
+    };
+  },
+
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios.get(this.apiUrl).then((response) => {
+        const result = response.data.response;
+        this.emailAddress.push(result);
+      });
+    }
+  },
+}).mount('#app');
